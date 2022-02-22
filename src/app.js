b0e42af3ddc2804a8425a06bba73a913b7143f62
Franco-Carrera <carrerafranco825@gmail.com>
2022-02-22 17:02:10 -0300

    minor changes of stagging

--- src/app.js
+++ src/app.js
@@ -1,13 +1,19 @@
 import express from "express";
 import { __dirname } from "./utils.js";
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT; // || 8080;
 
 app.use(express.static(__dirname + "/public"));
 
 app.get("/info", (req, res) => {
   res.send({ PORT: PORT });
+});
+
+app.get("/info2", (req, res) => {
+  res.send({ message: "Hola soy Fran" });
 });
 
 app.listen(PORT, () => console.log(`Listening on ${PORT} port.`));
